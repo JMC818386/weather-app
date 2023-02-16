140b512d5e56734fba4ebf81478844eca64a0dfd
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-16 02:34:29 +0000

    Declared api key

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={68793cbcde883e015990d3ae3a5f2cee}';
+const url = 'https://api.openweathermap.org/data/2.ZZZZ?zip={zip code},{country code}&appid={}';
+const apiKey = '68793cbcde883e015990d3ae3a5f2cee';
 
 axios.get(url)
   .then(function (response) {
