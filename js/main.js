c451fb697e241381b364acf3f71486d8770d6880
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-16 16:50:24 +0000

    Stuck on linking getWeatherBtn to axios.get Stuck on populateText function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,71 @@
+//Declare globally scoped variable to target input id
+let inputField = document.getElementById("inputField");
 
-const url = 'https://api.openweathermap.org/data/2.ZZZZ?zip={zip code},{country code}&appid={}';
-const apiKey = '68793cbcde883e015990d3ae3a5f2cee';
+//Figure out how to assign value from input field zipCode variable
+//Possible string literal or string cat
+let zipCode;
 
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+//Declare globally scoped variable to target input id
+let getWeatherBtn = document.getElementById("getWeatherBtn");
 
-  .then(function () {
-    // always executed
-  });
+//Declare globally scoped variables to target html id's
+let cityText = document.getElementById("cityText");
+let kelvinText = document.getElementById("kelvinText");
+let farenText = document.getElementById("farenText");
+let celsiusText = document.getElementById("celsiusText");
+let conditionText = document.getElementById("conditionText");
+let otherInfoText = document.getElementById("otherInfoText");
+
+
+//Delcare variable holding api url
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=40517,us&appid=68793cbcde883e015990d3ae3a5f2cee';
+//Delcare variable holding api key
+/* const apiKey = '68793cbcde883e015990d3ae3a5f2cee'; */
+
+//Initialize function to display inputField and getWeatherBtn on page load
+//function onLoad() {
+  //initializes on page load
+  //hides html outputContainer, shows html inputContainer
+
+//}
+
+
+//function getWeatherData() {
+    //If zipCode input doesn't work globally, place locally in function
+    //possible gotcha**
+  //Run axios function to get api data
+  axios.get(url)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      populateText(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+//}
+
+  
+//Create function to populate html id's (populateText)
+function populateText(arr) {
+  for(cityField of arr) {
+  //Variable city creates p element
+  let city = document.createElement('p');
+  //Weather name data is passed as string into p
+  city.innerText = `${cityField.name}`;
+  console.log(populateText);
+  city.appendChild(city);
+  }
+}
+
+  //Event listener on getWeatherBtn click
+  document.getElementById("getWeatherBtn").addEventListener("click", getWeatherData);
+    //takes zipcode from input
+    //gets data from api
+    //runs populateText function
+
+//
